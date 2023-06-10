ecd06c2a5afd1d4670d125aad01776c93b077ac2
lifengfengbb <lifengfengbb@gmail.com>
2023-06-10 10:23:15 +1000

    update

--- test_image.py
+++ test_image.py
@@ -8,10 +8,14 @@ load_dotenv()
 
 # settings and keys
 openai.api_key = os.environ.get('OPENAI_API_KEY')  # Assign OpenAI API key from environment variable
-model_engine = "gpt-3.5-turbo"                  # text-davinci-003 Define the model engine to be used
-language_out = 'zh'     #en                               # Define the language to be used by Google Text-to-Speech
-language_in = 'zh'
 
-audio_file= open("C:/Users/lifen/github/Smart-Speaker/recording_zh.mp3", "rb")
-transcript = openai.Audio.transcribe("whisper-1", audio_file)
-print(transcript["text"])
+
+input_prompt = "A evening photo taken at Sydney harbour bridge, Australia. The weather is clear sky and 14 degrees. The date is Queen's birthday (event). Near by there are people walking, cars and train on the bridge."
+
+response = openai.Image.create(
+  prompt=input_prompt,
+  n=1,
+  size="1024x1024"
+)
+image_url = response['data'][0]['url']
+print(image_url)
